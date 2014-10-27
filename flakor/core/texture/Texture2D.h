677b9d0a53fb1380a97dfb6833bfc6d77045bb20
Saint Hsu <saint@aliyun.com>
2014-10-27 21:15:46 +0800

    add doxygen

--- flakor/core/texture/Texture2D.h
+++ flakor/core/texture/Texture2D.h
@@ -4,54 +4,105 @@
 
 FLAKOR_NS_START
 
-/** @typedef CCTexture2DPixelFormat
-Possible texture pixel formats
-*/
+/** @typedef Texture2DPixelFormat
+  Possible texture pixel formats
+ */
 typedef enum {
+	//! auto detect the type
+	AUTO,
+	//! 32-bit texture: BGRA8888
+	BGRA8888,
+	//! 32-bit texture: RGBA8888
+	RGBA8888,
+	//! 24-bit texture: RGBA888
+	RGB888,
+	//! 16-bit texture without Alpha channel
+	RGB565,
+	//! 8-bit textures used as masks
+	A8,
+	//! 8-bit intensity texture
+	I8,
+	//! 16-bit textures used as masks
+	AI88,
+	//! 16-bit textures: RGBA4444
+	RGBA4444,
+	//! 16-bit textures: RGB5A1
+	RGB5A1,
+	//! 4-bit PVRTC-compressed texture: PVRTC4
+	PVRTC4,
+	//! 4-bit PVRTC-compressed texture: PVRTC4 (has alpha channel)
+	PVRTC4A,
+	//! 2-bit PVRTC-compressed texture: PVRTC2
+	PVRTC2,
+	//! 2-bit PVRTC-compressed texture: PVRTC2 (has alpha channel)
+	PVRTC2A,
+	//! ETC-compressed texture: ETC
+	ETC,
+	//! S3TC-compressed texture: S3TC_Dxt1
+	S3TC_DXT1,
+	//! S3TC-compressed texture: S3TC_Dxt3
+	S3TC_DXT3,
+	//! S3TC-compressed texture: S3TC_Dxt5
+	S3TC_DXT5,
+	//! ATITC-compressed texture: ATC_RGB
+	ATC_RGB,
+	//! ATITC-compressed texture: ATC_EXPLICIT_ALPHA
+	ATC_EXPLICIT_ALPHA,
+	//! ATITC-compresed texture: ATC_INTERPOLATED_ALPHA
+	ATC_INTERPOLATED_ALPHA,
+	//! Default texture format: AUTO
+	DEFAULT = AUTO,
 
-    //! 32-bit texture: RGBA8888
-    Texture2DPixelFormat_RGBA8888,
-    //! 24-bit texture: RGBA888
-    Texture2DPixelFormat_RGB888,
-    //! 16-bit texture without Alpha channel
-    Texture2DPixelFormat_RGB565,
-    //! 8-bit textures used as masks
-    Texture2DPixelFormat_A8,
-    //! 8-bit intensity texture
-    Texture2DPixelFormat_I8,
-    //! 16-bit textures used as masks
-    Texture2DPixelFormat_AI88,
-    //! 16-bit textures: RGBA4444
-    Texture2DPixelFormat_RGBA4444,
-    //! 16-bit textures: RGB5A1
-    Texture2DPixelFormat_RGB5A1,    
-    //! 4-bit PVRTC-compressed texture: PVRTC4
-    Texture2DPixelFormat_PVRTC4,
-    //! 2-bit PVRTC-compressed texture: PVRTC2
-    Texture2DPixelFormat_PVRTC2,
-
-
-    //! Default texture format: RGBA8888
-    Texture2DPixelFormat_Default = Texture2DPixelFormat_RGBA8888,
-
+	NONE = -1
 } Texture2DPixelFormat;
 
 typedef struct _TexParams {
-		    GLuint    minFilter;
-		    GLuint    magFilter;
-		    GLuint    wrapS;
-			GLuint    wrapT;
+	GLuint    minFilter;
+	GLuint    magFilter;
+	GLuint    wrapS;
+	GLuint    wrapT;
 } TexParams;
 
 class Texture2D : public Object,ITexture2D
 {
-    public:
+	public:
 		Texture2D();
 		virtual ~Texture2D();
-	
-	protexted:
-		unsigned int textureID;
+		bool initWithData(const void *data,int width,int height,Size size);
+		bool initWithFile(const char *fileName);
+		void setContentSize(const Size *size);
+		Size* getContentSize();
+		void load();
+		void unload();
+
+protexted:
+
 		Texture2DPixelFormat pixelFormat;
+		/** width in pixels */
+		int _pixelsWidth;
+
+		/** height in pixels */
+		int _pixelsHeight;
+
+		/** texture id in gpu */
+		GLuint _textureID
+
+		/** texture max S */
+		GLfloat _maxS;
+
+		/** texture max T */
+		GLfloat _maxT;
+
+		/** content size */
+		Size _contentSize;
+
+		/** whether or not the texture has their Alpha premultiplied */
+		bool _hasPremultipliedAlpha;
+
+		bool _hasMipmaps;
+
+		/** shader program used by drawAtPoint and drawInRect */
+		GLProgram* _shaderProgram;
 }
 
 FLAKOR_NS_END
